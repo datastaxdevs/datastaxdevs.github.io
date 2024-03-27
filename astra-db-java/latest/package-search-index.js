03157d9af029e3e7c22485c2299ebca905e44651
Cedrick Lunven <cedrick.lunven@gmail.com>
2024-03-27 15:06:46 +0100

    doc

---
+++ astra-db-java/latest/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.datastax.astra.client"},{"l":"com.datastax.astra.client.admin"},{"l":"com.datastax.astra.client.exception"},{"l":"com.datastax.astra.client.model"},{"l":"com.datastax.astra.internal"},{"l":"com.datastax.astra.internal.utils"}];updateSearchResults();
